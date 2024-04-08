ddd34a61b9646ee444a78fee6e02860193a234c5
ege <egecam000@gmail.com>
2024-04-09 01:00:30 +0300

    Create SplashView.swift

---
+++ Vizor/SplashView.swift
@@ -0,0 +1,51 @@
+//
+//  SplashView.swift
+//  Vizor
+//
+//  Created by Ege Çam on 9.04.2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State var animateGradient: Bool = false
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if self.isActive {
+                ContentView()
+            } else {
+                ZStack {
+                    Color.black
+                    
+                    LinearGradient(colors: [.orange, .pink], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .hueRotation(.degrees(animateGradient ? 0 : 180))
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 3.5)) {
+                                animateGradient.toggle()
+                            }
+                        }
+                        .ignoresSafeArea()
+                    
+                    Label("Vizor", systemImage: "dot.viewfinder")
+                        .font(.bricolageGrotesque(size: 48, width: 100, weight: 600))
+                        .bold()
+                        .padding()
+                }
+                .ignoresSafeArea()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation(.easeOut(duration: 0.2)) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
