35f00f657a5f90eaa94c51526f6fd0d300763940
ege <egecam000@gmail.com>
2024-04-14 17:06:16 +0300

    Move file to View directory

--- Vizor/View/ContentView.swift
+++ Vizor/View/ContentView.swift
@@ -10,26 +10,31 @@ import SwiftData
 import VFont
 
 struct ContentView: View {
-    @State private var selectedTab = 1
-    @State private var animateGradient: Bool = false
+    @State var currentPhase: String = "Golden Hour"
+    @State var themeColor: Color = colors["Golden Hour"] ?? .cyan
+    
+    @State var dynamicThemePreference: Bool = true
+    
+    @State var stateTheme: Color = .cyan
     
     var body: some View {
         ZStack {
-            
             // SCROLLVIEW FOR TABS
             ScrollView(.horizontal) {
                 LazyHStack(spacing: 0) {
                     VCalendar()
                         .containerRelativeFrame(.horizontal)
                     
-                    Phases()
-                        .containerRelativeFrame(.horizontal)
-                        .scrollTransition(axis: .horizontal) { content, phase in
-                            content
-                                .scaleEffect(x: phase.isIdentity ? 1 : 0.8, y: phase.isIdentity ? 1 : 0.8)
-                        }
+                    ZStack {
+                        Phases(currentPhase: $currentPhase, themeColor: $themeColor)
+                            .containerRelativeFrame(.horizontal)
+                            .scrollTransition(axis: .horizontal) { content, phase in
+                                content
+                                    .scaleEffect(x: phase.isIdentity ? 1 : 0.8, y: phase.isIdentity ? 1 : 0.8)
+                            }
+                    }
                     
-                    Feed()
+                    Feed(colorTheme: setTheme(dynamicPreference: dynamicThemePreference, theme: colors[currentPhase]!))
                         .containerRelativeFrame(.horizontal)
                 }
             }
