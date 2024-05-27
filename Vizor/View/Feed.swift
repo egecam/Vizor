055cad709ef82f53d17c942e2f4dc86b709dc356
ege <egecam000@gmail.com>
2024-05-27 23:28:37 +0300

    feature(ui): very huge reforms in feed. gonna improve soon

--- Vizor/View/Feed.swift
+++ Vizor/View/Feed.swift
@@ -12,60 +12,20 @@ struct Feed: View {
     @State var colorTheme: Color
     
     var body: some View {
-        
-        ZStack {
-            Color.white
-                .opacity(0.2)
-                .ignoresSafeArea()
-            
-            ScrollView {
-                ForEach(0..<20) { post in
-                    VStack(alignment: .leading) {
-                        Text("Leica SL3 Review")
-                            .modifier(ArticleTitle())
-                            .foregroundStyle(colorTheme)
-                        
-                        Text("The Leica SL3 (Type 5404) is a 35mm full-frame interchangeable lens mirrorless camera designed to appeal to both stills photographers and videographers.")
-                            .modifier(ArticleBody())
-                        
-                        HStack {
-                            
-                            Button {
-                                // like
-                            } label: {
-                                Label("18 Likes", systemImage: "heart")
-                            }
-                            .tint(.primary)
-                            
-                            
-                            Spacer()
-                            
-                            Button {
-                                // comments section
-                            } label: {
-                                ZStack {
-                                    colorTheme.opacity(0.15)
-                                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
-                                    Label("9 Comments", systemImage: "message")
-                                }
-                                .frame(width: 150, height: 35)
-                            }
-                            .tint(.primary)
-                            
-                            
-                            
-                            Spacer()
-                            
-                        }
-                        .bold()
-                        .padding(5)
+        ScrollView {
+            Grid {
+                ForEach(1..<9) { _ in
+                    GridRow {
+                        FeedElement(thumbnailPhoto: "photobw", aspectRatio: 0.8)
+                            .padding(4)
+                        FeedElement(thumbnailPhoto: "photocolor", aspectRatio: 0.8)
+                            .padding(4)
                     }
-                    .offset(x: 0, y: 200)
-                    .padding()
+                    .scaledToFit()
                 }
             }
-            
         }
+        .offset(x: 0, y: 120)
     }
 }
 
