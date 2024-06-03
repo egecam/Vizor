01fcaac2955674a15f8d6324220918e4bb01e112
ege <egecam000@gmail.com>
2024-06-03 18:21:34 +0300

    updated firebase handler

--- Vizor/Firebase/FirebaseHandler.swift
+++ Vizor/Firebase/FirebaseHandler.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+/*
+func signUp(username: String, password: String, email: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        if let error = error {
+            completion(.failure(error))
+        } else {
+            guard let uid = authResult?.user.uid else { return }
+            let db = Firestore.firestore()
+            db.collection("users").document(uid).setData([
+                "username": username,
+                "email": email
+            ]) { error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(()))
+                }
+            }
+        }
+    }
+}
+*/
