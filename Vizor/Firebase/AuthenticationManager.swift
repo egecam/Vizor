8f80593a64f6cfa0b8b7a05c416d529bff6eae18
ege <egecam000@gmail.com>
2024-06-13 16:59:41 +0300

    fix(api): fixed Firebase authentication

--- Vizor/Firebase/AuthenticationManager.swift
+++ Vizor/Firebase/AuthenticationManager.swift
@@ -22,9 +22,10 @@ final class AuthenticationManager {
     static let shared = AuthenticationManager()
     private init() { }
     
-    func getAuthenticatedUser() throws -> AuthDataResultModel {
+    // Oturum açmış bir kullanıcı varsa AuthDataResultModel döndürür, yoksa nil döndürür
+    func getAuthenticatedUser() throws -> AuthDataResultModel? {
         guard let user = Auth.auth().currentUser else {
-            throw URLError(.badServerResponse)
+            return nil
         }
         
         return AuthDataResultModel(user: user)
@@ -33,6 +34,12 @@ final class AuthenticationManager {
     @discardableResult
     func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    @discardableResult
+    func signIn(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
         return AuthDataResultModel(user: authDataResult.user)
     }
     
